c2d6dd3bbed43f2de18baae80133604ba21ddc09
Ahmed Hany <dev.ahmedhany@gmail.com>
2022-12-12 12:56:02 +0200

    removing mui icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,11 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Box } from "@mui/material";
-import { Person } from "@mui/icons-material";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Box>
       <Component {...pageProps} />
-      <Person />
     </Box>
   );
 }
